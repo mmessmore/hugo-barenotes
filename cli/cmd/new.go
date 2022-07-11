fe93513568cbc0a1935ed85bcc68fca87a3769bd
Michael Messmore <mike@messmore.org>
2022-07-11 10:49:26 -0500

    Starting a CLI to drive using the theme as a tool

---
+++ cli/cmd/new.go
@@ -0,0 +1,49 @@
+/*
+Copyright © 2022 Mike Messmore <mike@messmore.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"strings"
+
+	"github.com/mmessmore/hugo-barenotes/cli/internal"
+	"github.com/spf13/cobra"
+)
+
+// newCmd represents the new command
+var newCmd = &cobra.Command{
+	Args:    cobra.MinimumNArgs(1),
+	Use:     "new [TITLE OF NOTE]",
+	Aliases: []string{"create"},
+	Short:   "Create a new note",
+	Long: `Create a new note.
+
+The title can be 1 or more aguments joined by spaces`,
+	Run: func(cmd *cobra.Command, args []string) {
+		title := strings.Join(args, " ")
+		internal.CD()
+		internal.Create(title)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(newCmd)
+}
---
+++ cli/internal/new.go
@@ -0,0 +1,85 @@
+package internal
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+
+	"github.com/spf13/viper"
+)
+
+func CD() {
+	dir := viper.GetString("root")
+	err := os.Chdir(dir)
+	if err != nil {
+		fmt.Printf("ERROR: Could not change directory to %s\n", dir)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func Create(title string) {
+
+	filename := fmt.Sprintf("notes/%s.md",
+		strings.ReplaceAll(strings.ToLower(title), " ", "-"))
+
+	hugo, err := exec.LookPath("hugo")
+	if err != nil {
+		fmt.Println("ERROR: Could not find hugo executable")
+		os.Exit(1)
+	}
+
+	cmd := exec.Command(hugo, "new", filename)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println("ERROR: Failed to run hugo")
+		fmt.Println(out)
+	}
+
+	editor, err := GetEditor()
+	if err != nil {
+		fmt.Println("ERROR: Could not find an editor to execute")
+		os.Exit(1)
+	}
+	filename = fmt.Sprintf("content/%s", filename)
+	env := os.Environ()
+
+	err = syscall.Exec(editor, []string{filename}, env)
+	if err != nil {
+		fmt.Printf("ERROR: failed to open editor on %s\n", filename)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func GetEditor() (string, error) {
+	var err error
+	editor := viper.GetString("editor")
+	if editor == "" {
+		editor = os.Getenv("VISUAL")
+	}
+	if editor == "" {
+		editor = os.Getenv("EDITOR")
+	}
+	if editor == "" {
+		editor, err = exec.LookPath("editor")
+		if err != nil {
+			editor = ""
+		}
+	}
+	if editor == "" {
+		editor, err = exec.LookPath("vi")
+		if err != nil {
+			editor = ""
+		}
+	}
+
+	if editor == "" {
+		return "", errors.New("Could not find an editor to use")
+	}
+
+	return editor, nil
+}
